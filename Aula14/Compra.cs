54d59359560d40fd3bdfc1b113e58642fe1bd1ba
caiopgaldino <51961019+caiopgaldino@users.noreply.github.com>
2021-05-30 13:48:40 -0300

    Add files via upload

---
+++ Aula14/Compra.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tarefa_aula_14
+{
+   public class Compra: IImprimivel
+    {
+        private Cliente _cliente;
+        private Carrinho _carrinho;
+        public Cliente Cliente
+        {
+            get
+            {
+                return this._cliente;
+            }
+        }
+
+        public Carrinho Carrinho
+        {
+            get
+            {
+                return this._carrinho;
+            }
+        }
+        public Compra(Cliente cliente, Carrinho carrinho)
+        {
+            this._cliente = cliente;
+            this._carrinho = carrinho;
+        }
+        public void Imprimir()
+        {
+            this.Cliente.Imprimir();
+            this.Carrinho.ImprimirCarrinho();
+        }
+    }
+}
---
+++ Aula14/Fornecedores.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tarefa_aula_14
+{
+   public class Fornecedores : IImprimivel
+    {
+        private string _nome;
+        private string _cnpj;
+        private List<Produto> _item;
+        public string Nome
+        {
+            get
+            {
+                return this._nome;
+            }
+        }
+
+        public string Cnpj
+        {
+            get
+            {
+                return this._cnpj;
+            }
+        }
+        public Fornecedores(string nome, string cnpj, List<Produto>produtos)
+        {
+            this._nome = nome;
+            this._cnpj = cnpj;
+            this._item = produtos;
+        }
+        public void Imprimir()
+        {
+            Console.WriteLine("Nome:\t{0}", this.Nome);
+            Console.WriteLine("CNPJ:\t{0}", this.Cnpj);
+            foreach (var item in _item)
+            {
+                item.Imprimir();
+                Console.WriteLine("---------------------------");
+            }
+        }
+    }
+}
